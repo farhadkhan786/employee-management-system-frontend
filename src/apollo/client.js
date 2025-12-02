fd5b4796a6d3f9ed67bd823cb86179d9db756473
Farhad Hussain Khan <78996980+farhadkhan786@users.noreply.github.com>
2025-12-02 18:15:31 +0500

    Update client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: 'https://employee-management-system-backend-x01b.onrender.com/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
